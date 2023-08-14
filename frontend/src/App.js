f5f73ec8a01df2d15dc520970a417eae13a051e8
Kyle Coberly <kyle.coberly@gmail.com>
2023-08-14 14:01:11 -0600

    Add static listings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Barkwire</h1>
+			<p>A dating app for dogs</p>
+			<ul>
+				<li>
+					<div className="DogListing">
+						<p>Bixby</p>
+						<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rd.com%2Fwp-content%2Fuploads%2F2020%2F12%2FGettyImages-185184089.jpg&f=1&nofb=1&ipt=9fe7ee5c4e47bec58ae162811e773ca764092c7a9aca3875b5ebd3e8bd1e0b58&ipo=images" alt="Bixby" />
+					</div>
+				</li>
+				<li>
+					<div className="DogListing">
+						<p>Bixby</p>
+						<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rd.com%2Fwp-content%2Fuploads%2F2020%2F12%2FGettyImages-185184089.jpg&f=1&nofb=1&ipt=9fe7ee5c4e47bec58ae162811e773ca764092c7a9aca3875b5ebd3e8bd1e0b58&ipo=images" alt="Bixby" />
+					</div>
+				</li>
+				<li>
+					<div className="DogListing">
+						<p>Bixby</p>
+						<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rd.com%2Fwp-content%2Fuploads%2F2020%2F12%2FGettyImages-185184089.jpg&f=1&nofb=1&ipt=9fe7ee5c4e47bec58ae162811e773ca764092c7a9aca3875b5ebd3e8bd1e0b58&ipo=images" alt="Bixby" />
+					</div>
+				</li>
+				<li>
+					<div className="DogListing">
+						<p>Bixby</p>
+						<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rd.com%2Fwp-content%2Fuploads%2F2020%2F12%2FGettyImages-185184089.jpg&f=1&nofb=1&ipt=9fe7ee5c4e47bec58ae162811e773ca764092c7a9aca3875b5ebd3e8bd1e0b58&ipo=images" alt="Bixby" />
+					</div>
+				</li>
+			</ul>
+		</div>
+	);
 }
 
 export default App;
