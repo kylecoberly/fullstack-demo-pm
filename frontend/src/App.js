fe73e3efec662a21f3efd2b5246de5780bed43ea
Kyle Coberly <kyle.coberly@gmail.com>
2023-08-14 14:10:05 -0600

    Style dog list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,31 +3,39 @@ import './App.css';
 function App() {
 	return (
 		<div className="App">
-			<h1>Barkwire</h1>
-			<p>A dating app for dogs</p>
-			<ul>
+			<header>
+				<h1>Barkwire</h1>
+				<p>A dating app for dogs</p>
+			</header>
+			<ul className="DogList">
 				<li>
 					<div className="DogListing">
-						<p>Bixby</p>
 						<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rd.com%2Fwp-content%2Fuploads%2F2020%2F12%2FGettyImages-185184089.jpg&f=1&nofb=1&ipt=9fe7ee5c4e47bec58ae162811e773ca764092c7a9aca3875b5ebd3e8bd1e0b58&ipo=images" alt="Bixby" />
+						<p>Bixby</p>
 					</div>
 				</li>
 				<li>
 					<div className="DogListing">
-						<p>Bixby</p>
 						<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rd.com%2Fwp-content%2Fuploads%2F2020%2F12%2FGettyImages-185184089.jpg&f=1&nofb=1&ipt=9fe7ee5c4e47bec58ae162811e773ca764092c7a9aca3875b5ebd3e8bd1e0b58&ipo=images" alt="Bixby" />
+						<p>Bixby</p>
 					</div>
 				</li>
 				<li>
 					<div className="DogListing">
-						<p>Bixby</p>
 						<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rd.com%2Fwp-content%2Fuploads%2F2020%2F12%2FGettyImages-185184089.jpg&f=1&nofb=1&ipt=9fe7ee5c4e47bec58ae162811e773ca764092c7a9aca3875b5ebd3e8bd1e0b58&ipo=images" alt="Bixby" />
+						<p>Bixby</p>
 					</div>
 				</li>
 				<li>
 					<div className="DogListing">
-						<p>Bixby</p>
 						<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rd.com%2Fwp-content%2Fuploads%2F2020%2F12%2FGettyImages-185184089.jpg&f=1&nofb=1&ipt=9fe7ee5c4e47bec58ae162811e773ca764092c7a9aca3875b5ebd3e8bd1e0b58&ipo=images" alt="Bixby" />
+						<p>Bixby</p>
+					</div>
+				</li>
+				<li>
+					<div className="DogListing">
+						<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rd.com%2Fwp-content%2Fuploads%2F2020%2F12%2FGettyImages-185184089.jpg&f=1&nofb=1&ipt=9fe7ee5c4e47bec58ae162811e773ca764092c7a9aca3875b5ebd3e8bd1e0b58&ipo=images" alt="Bixby" />
+						<p>Bixby</p>
 					</div>
 				</li>
 			</ul>
